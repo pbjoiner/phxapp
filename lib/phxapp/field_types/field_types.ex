9e4ca87f7177c38d1a897f2354062a5fada515b4
Paul B. Joiner <paulj@engageyourcause.com>
2020-07-23 20:27:40 -0500

    add delete cascade to field options

--- lib/phxapp/field_types/field_types.ex
+++ lib/phxapp/field_types/field_types.ex
@@ -1,104 +1,107 @@
 defmodule Phxapp.FieldTypes do
-  @moduledoc """
-  The FieldTypes context.
-  """
+	@moduledoc """
+	The FieldTypes context.
+	"""
 
-  import Ecto.Query, warn: false
-  alias Phxapp.Repo
+	import Ecto.Query, warn: false
+	alias Phxapp.Repo
 
-  alias Phxapp.FieldTypes.FieldType
+	alias Phxapp.FieldTypes.FieldType
 
-  @doc """
-  Returns the list of field_types.
+	@doc """
+	Returns the list of field_types.
 
-  ## Examples
+	## Examples
 
-      iex> list_field_types()
-      [%FieldType{}, ...]
+	    iex> list_field_types()
+	    [%FieldType{}, ...]
 
-  """
-  def list_field_types do
-    Repo.all(FieldType)
-  end
+	"""
+	def list_field_types do
+		Repo.all(FieldType)
+	end
 
-  @doc """
-  Gets a single field_type.
+	@doc """
+	Gets a single field_type.
 
-  Raises `Ecto.NoResultsError` if the Field type does not exist.
+	Raises `Ecto.NoResultsError` if the Field type does not exist.
 
-  ## Examples
+	## Examples
 
-      iex> get_field_type!(123)
-      %FieldType{}
+	    iex> get_field_type!(123)
+	    %FieldType{}
 
-      iex> get_field_type!(456)
-      ** (Ecto.NoResultsError)
+	    iex> get_field_type!(456)
+	    ** (Ecto.NoResultsError)
 
-  """
-  def get_field_type!(id), do: Repo.get!(FieldType, id)
+	"""
+	#  def get_field_type!(id), do: Repo.get!(FieldType, id)
+	def get_field_type!(id) do
+		Repo.all(from(ft in FieldType, join: fo in assoc(ft, :field_options), where: ft.id == ^id, preload: [field_options: fo]))
+	end
 
-  @doc """
-  Creates a field_type.
+	@doc """
+	Creates a field_type.
 
-  ## Examples
+	## Examples
 
-      iex> create_field_type(%{field: value})
-      {:ok, %FieldType{}}
+	    iex> create_field_type(%{field: value})
+	    {:ok, %FieldType{}}
 
-      iex> create_field_type(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+	    iex> create_field_type(%{field: bad_value})
+	    {:error, %Ecto.Changeset{}}
 
-  """
-  def create_field_type(attrs \\ %{}) do
-    %FieldType{}
-    |> FieldType.changeset(attrs)
-    |> Repo.insert()
-  end
+	"""
+	def create_field_type(attrs \\ %{}) do
+		%FieldType{}
+		|> FieldType.changeset(attrs)
+		|> Repo.insert()
+	end
 
-  @doc """
-  Updates a field_type.
+	@doc """
+	Updates a field_type.
 
-  ## Examples
+	## Examples
 
-      iex> update_field_type(field_type, %{field: new_value})
-      {:ok, %FieldType{}}
+	    iex> update_field_type(field_type, %{field: new_value})
+	    {:ok, %FieldType{}}
 
-      iex> update_field_type(field_type, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+	    iex> update_field_type(field_type, %{field: bad_value})
+	    {:error, %Ecto.Changeset{}}
 
-  """
-  def update_field_type(%FieldType{} = field_type, attrs) do
-    field_type
-    |> FieldType.changeset(attrs)
-    |> Repo.update()
-  end
+	"""
+	def update_field_type(%FieldType{} = field_type, attrs) do
+		field_type
+		|> FieldType.changeset(attrs)
+		|> Repo.update()
+	end
 
-  @doc """
-  Deletes a field_type.
+	@doc """
+	Deletes a field_type.
 
-  ## Examples
+	## Examples
 
-      iex> delete_field_type(field_type)
-      {:ok, %FieldType{}}
+	    iex> delete_field_type(field_type)
+	    {:ok, %FieldType{}}
 
-      iex> delete_field_type(field_type)
-      {:error, %Ecto.Changeset{}}
+	    iex> delete_field_type(field_type)
+	    {:error, %Ecto.Changeset{}}
 
-  """
-  def delete_field_type(%FieldType{} = field_type) do
-    Repo.delete(field_type)
-  end
+	"""
+	def delete_field_type(%FieldType{} = field_type) do
+		Repo.delete(field_type)
+	end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking field_type changes.
+	@doc """
+	Returns an `%Ecto.Changeset{}` for tracking field_type changes.
 
-  ## Examples
+	## Examples
 
-      iex> change_field_type(field_type)
-      %Ecto.Changeset{data: %FieldType{}}
+	    iex> change_field_type(field_type)
+	    %Ecto.Changeset{data: %FieldType{}}
 
-  """
-  def change_field_type(%FieldType{} = field_type, attrs \\ %{}) do
-    FieldType.changeset(field_type, attrs)
-  end
+	"""
+	def change_field_type(%FieldType{} = field_type, attrs \\ %{}) do
+		FieldType.changeset(field_type, attrs)
+	end
 end
--- priv/repo/migrations/20200716220314_create_field_options.exs
+++ priv/repo/migrations/20200716220314_create_field_options.exs
@@ -5,7 +5,7 @@ defmodule Phxapp.Repo.Migrations.CreateFieldOptions do
     create table(:field_options) do
       add :label, :string, null: false
       add :value, :string, null: false
-      add :field_type_id, :id, null: false
+      add :field_type_id, references(:field_types, on_delete: :delete_all)
     end
   end
 
